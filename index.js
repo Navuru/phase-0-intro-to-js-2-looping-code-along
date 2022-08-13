1c91360da95c76bf1b92d883b860d89e55da3aa0
Lisa Lyayuga <lisalyayuga@gmail.com>
2022-08-13 14:34:16 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+function writeCards(names, msg) {
+
+    let message = [] 
+
+    for (let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${msg} gift!`)
+    }
+    return message;
+}
+
+
+function countDown(number){
+
+    if(number <= 0){
+        return
+    }
+
+    for (let i = number; i >= 0 ;  i--){
+        console.log(i);
+    }
+}
+
